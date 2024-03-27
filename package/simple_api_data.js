a3add40a08bebdadcc0c06029ee0b426ab5ee489
Aachman Mittal <m.aachman@gmail.com>
2024-03-27 12:44:48 +0530

    Restructure dir, add simple api

---
+++ package/simple_api_data.js
@@ -0,0 +1,9 @@
+const data = [
+  { name: "Aachman Mittal", email: "Aachman@example.com" },
+  { name: "Aachman Mittal", email: "Aachman@example.com" },
+  { name: "Aachman Mittal", email: "Aachman@example.com" },
+  { name: "Aachman Mittal", email: "Aachman@example.com" },
+  { name: "Aachman Mittal", email: "Aachman@example.com" },
+];
+
+module.exports = data;
